279cc122dd2db04f4a68871827429021b190c677
t-willi-28 <taku.0628william@gmail.com>
2021-05-18 10:59:42 +0900

    Rspecの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,13 @@ class User < ApplicationRecord
   validates :password, 
     format: {with: VALID_PASSWORD_REGEX}
   validates :nickname, presence: true
-  validates :first_name, presence: true
+  validates :first_name, presence: true,
     format: { with:  /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
-  validates :last_name, presence: true
+  validates :last_name, presence: true,
     format: { with:  /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
-  validates :first_name_kana, presence: true
+  validates :first_name_kana, presence: true,
   format: { with:  /\A[ァ-ヶ一]+\z/}
-  validates :last_name_kana, presence: true
+  validates :last_name_kana, presence: true,
   format: { with:  /\A[ァ-ヶ一]+\z/}
   validates :birth_date, presence: true
 end
