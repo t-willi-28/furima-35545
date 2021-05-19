9c29a3549f2ac5628bff53939cd26e5296bda8d7
t-willi-28 <taku.0628william@gmail.com>
2021-05-19 12:13:38 +0900

    バリデーションの設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,15 @@ class Product < ApplicationRecord
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :from
+
+  with_options presence: true
+    validates :name
+    validates :description
+    validates :category_id
+    validates :stat_id
+    validates :from_id, numericality: { other_than: 1 }
+    validates :shipping_day_id
+    validates :cost_id
+    validates :price
+  end
 end
