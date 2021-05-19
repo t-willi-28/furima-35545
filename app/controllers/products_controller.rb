2ec183fcb584bf846f9914f04ef6fc1313ecad96
t-willi-28 <taku.0628william@gmail.com>
2021-05-19 15:21:45 +0900

    アクションとルーティングの設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,12 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def create
+    Product.create(product_params)
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :description, :category_id, :stat_id, :from_id, :shipping_day_id, :cost_id, :price).merge(user_id: current_user.id)
+  end
 end
