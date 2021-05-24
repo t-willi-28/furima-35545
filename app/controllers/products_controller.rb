9c22dc84603aed6db7fc653daf293d4d40d54ed3
t-willi-28 <taku.0628william@gmail.com>
2021-05-24 14:42:48 +0900

    viewファイルの配置

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,6 +21,10 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
   private
 
   def product_params
