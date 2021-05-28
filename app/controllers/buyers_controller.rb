fb29470a75a97de59f1f31f3cc36334939562f01
t-willi-28 <taku.0628william@gmail.com>
2021-05-28 17:44:02 +0900

    購入機能の実装

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,6 +1,34 @@
 class BuyersController < ApplicationController
 
   def index
+    @product = Product.find(params[:product_id])
     @buyer_address = BuyerAddress.new
+  end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @buyer_address = BuyerAddress.new(buyer_params)
+    if @buyer_address.valid?
+      pay_product
+      @buyer_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def buyer_params
+    params.require(:buyer_address).permit(:post_code, :from_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, product_id: @product.id, token: params[:token])
+  end
+
+  def pay_product
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @product.price,
+      card: buyer_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/buyer_address.rb
+++ app/models/buyer_address.rb
@@ -1,17 +1,20 @@
 class BuyerAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :product_id, :post_code, :from_id, :municipality, :building_name, :address, :phone_number
+  attr_accessor :user_id, :product_id, :post_code, :from_id, :municipality, :building_name, :address, :phone_number, :token
 
   with_options presence: true do
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
-    validates :from_id, numericality: { other_than: 0 }
+    validates :from_id, numericality: { other_than: 1 }
     validates :municipality
     validates :address
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+    validates :token
+    validates :user_id
+    validates :product_id
   end
 
   def save
     buyer = Buyer.create(user_id: user_id, product_id: product_id)
-    Address.create(post_code: post_code, from_id: from_id, municipality: municipality, building_name: building_name, address: address, phone_number: phone_number, buyer_id: buyer.id)
+    ShipAddress.create(post_code: post_code, from_id: from_id, municipality: municipality, building_name: building_name, address: address, phone_number: phone_number, buyer_id: buyer.id)
   end
 end
