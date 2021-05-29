fa2e18542b55ffac045566ddebb4d0be56a3e6d0
t-willi-28 <taku.0628william@gmail.com>
2021-05-29 18:06:58 +0900

    コメントの修正３

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -52,6 +52,6 @@ class ProductsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path unless user_signed_in? || current_user.id == @product.user_id || @product.buyer.present?
+    redirect_to root_path unless current_user.id == @product.user_id || @product.buyer.present?
   end
 end
